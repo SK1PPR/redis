22121d454aaefc8eceeeab2d32bb3e26a5f90871
SK1PPR <agrawalkhushal04@gmail.com>
2025-08-05 13:10:12 +0530

    Testing reading infinite input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{Read, Write};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,7 +12,24 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                stream.write_all(b"+PONG\r\n").unwrap();
+
+                let mut buf = [0; 1024];
+                while stream.peek(buf.as_mut()).unwrap() > 0 {
+                    let bytes_read = stream.read(&mut buf).unwrap();
+                    if bytes_read == 0 {
+                        break;
+                    }
+                    
+                    // Process the command (for simplicity, we just echo it back)
+                    println!("Received: {}", String::from_utf8_lossy(&buf[..bytes_read]));
+
+                    // Break the bytes_read into multiple PING commands and send PONG responses for each
+                    buf.chunks_mut(5).for_each(|chunk| {
+                        if chunk.starts_with(b"PING") {
+                            stream.write_all(b"+PONG\r\n").unwrap();
+                        }
+                    });
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
