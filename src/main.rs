555c4591e6643e6c76ccb77bbb0e048632b3579e
SK1PPR <agrawalkhushal04@gmail.com>
2025-08-05 13:12:11 +0530

    Testing reading infinite input

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,14 @@ fn main() {
             Ok(mut stream) => {
                 println!("accepted new connection");
 
-                let mut buf = [0; 1024];
-                while stream.peek(buf.as_mut()).unwrap() > 0 {
-                    let bytes_read = stream.read(&mut buf).unwrap();
-                    if bytes_read == 0 {
+                let mut buf = [0; 512];
+                loop {
+                    let read_count = stream.read(&mut buf).unwrap();
+                    if read_count == 0 {
                         break;
                     }
-                    
-                    // Process the command (for simplicity, we just echo it back)
-                    println!("Received: {}", String::from_utf8_lossy(&buf[..bytes_read]));
 
-                    // Break the bytes_read into multiple PING commands and send PONG responses for each
-                    buf.chunks_mut(5).for_each(|chunk| {
-                        if chunk.starts_with(b"PING") {
-                            stream.write_all(b"+PONG\r\n").unwrap();
-                        }
-                    });
+                    stream.write(b"+PONG\r\n").unwrap();
                 }
             }
             Err(e) => {
